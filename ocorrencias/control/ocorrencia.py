552fdc4bd853a614f6d1e1ebbed4d657aeccbe4d
Thiago Abreu Rodrigues <thiago.ar17@gmail.com>
2015-05-27 01:24:58 -0300

    Alterando vizualizacao do mapa

--- ocorrencias/control/ocorrencia.py
+++ ocorrencias/control/ocorrencia.py
@@ -17,7 +17,7 @@ class Ocorrencia(osv.Model):
             latitude = dado.get('ocorrencia_latitude')
 
         url = "https://maps.googleapis.com/maps/api/staticmap?center="+\
-              latitude+","+longitude+"&markers="+latitude+"," + longitude \
+              longitude+","+latitude+"&markers="+longitude+"," + latitude \
               +"&zoom=16&size=640x640"
         # url = "http://www.google.com.br/maps/@%s,%s&markers=%s,%s&zoom=16" % (longitude, latitude,
         #                                                                       longitude, latitude)
